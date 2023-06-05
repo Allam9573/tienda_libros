c5ffab41f5537b1a845ed1323f83adebda8459b9
Allam9573 <aallamdaniel@gmail.com>
2023-06-05 11:49:11 -0600

    template books

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from app.modules.auth import auth
+from app.modules.books import books
 
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(auth.bp)
+    app.register_blueprint(books.bp)
 
     @app.route('/')
     def index():
