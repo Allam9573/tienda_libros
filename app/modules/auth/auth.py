7c9974315c7cc6fddd50742f3aad14b6625bb7a7
Allam9573 <aallamdaniel@gmail.com>
2023-06-05 10:08:10 -0600

    login y sign

--- app/modules/auth/auth.py
+++ app/modules/auth/auth.py
@@ -6,3 +6,8 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/login')
 def login():
     return render_template('auth/login.html')
+
+
+@bp.route('/signup')
+def signup():
+    return render_template('auth/signup.html')
