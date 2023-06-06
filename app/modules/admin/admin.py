6c3227c053fbb8323b63cae217e158ca0b80b198
Allam9573 <aallamdaniel@gmail.com>
2023-06-06 09:36:36 -0600

    formulario registro de libro y tabla de datos

--- app/modules/admin/admin.py
+++ app/modules/admin/admin.py
@@ -1,11 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
-bp = Blueprint('admin',__name__,url_prefix='/admin')
+bp = Blueprint('admin', __name__, url_prefix='/admin')
+
 
 @bp.route('/')
 def admin_home():
     return render_template('admin/index.html')
 
-@bp.route('/upload')
+
+@bp.route('/upload', methods=['POST', 'GET'])
 def upload():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        imagenURL = request.form['imagenURL']
+        url = request.form['url']
+        precio = request.form['precio']
     return render_template('admin/upload.html')
