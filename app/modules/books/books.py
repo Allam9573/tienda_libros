da3c64eea2df2e9c055630524c0fb8f6e66f586e
Allam9573 <aallamdaniel@gmail.com>
2023-06-07 10:07:54 -0600

    lista de libros en view

--- app/modules/books/books.py
+++ app/modules/books/books.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, render_template
+from app.modules.db.db import get_connection
 
-bp = Blueprint('books', __name__,url_prefix='/books')
+bp = Blueprint('books', __name__, url_prefix='/books')
+conexion = get_connection()
+
 
 @bp.route('/list')
 def list():
-    return render_template('books/books.html')
+    cursor = conexion.cursor(dictionary=True)
+    query = "SELECT * FROM libros"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    return render_template('books/books.html', data=data)
